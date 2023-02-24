fddc39188eda29fa01a90726fff5d9037e21a0a8
GrandvilleTanbunan <46751850+GrandvilleTanbunan@users.noreply.github.com>
2023-02-24 10:07:39 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SumberTeknik',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
