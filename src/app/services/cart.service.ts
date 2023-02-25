46446ee88738e3bb593c88a46b51452375e01883
GrandvilleTanbunan <46751850+GrandvilleTanbunan@users.noreply.github.com>
2023-02-25 11:00:10 +0800

    First commit

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  amount: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  data: Product[] = [
+    {id: 0, name: 'Pizza', price: 50000, amount: 1},
+    {id: 1, name: 'Hamburger', price: 25000, amount: 1},
+    {id: 2, name: 'Croissant', price: 10000, amount: 1},
+    {id: 3, name: 'Roti', price: 5000, amount: 1}
+  ];
+
+  private cart: any = [];
+  private cartItemCount = new BehaviorSubject(0);
+
+  constructor() { }
+
+  getProducts(){
+    return this.data;
+  }
+
+  getCart(){
+    return this.cart;
+  }
+
+  getCartItemCount(){
+    return this.cartItemCount;
+  }
+
+  addProduct(product: any){
+    let added = false;
+    for (let p of this.cart) {
+      if (p.id === product.id) {
+        p.amount += 1;
+        added = true;
+        break;
+      }
+    }
+    if (!added) {
+      product.amount = 1;
+      this.cart.push(product);
+    }
+    this.cartItemCount.next(this.cartItemCount.value + 1);
+  }
+
+  decreaseProduct(product: any){
+    for(let [index, p] of this.cart.entries()){
+      if(p.id === product.id)
+      {
+        p.amount -= 1;
+        if(p.amount == 0)
+        {
+          this.cart.splice(index, 1);
+        }
+      }
+    }
+    this.cartItemCount.next(this.cartItemCount.value - 1);
+  }
+
+  removeProduct(product: any){
+    for(let [index, p] of this.cart.entries()){
+      if(p.id === product.id)
+      {
+        this.cartItemCount.next(this.cartItemCount.value - p.amount);
+        this.cart.splice(index, 1);
+      }
+    }
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ToastController, ModalController } from '@ionic/angular';
+import { CartService } from '../services/cart.service';
+import { BehaviorSubject } from 'rxjs';
+import { CartModalPage } from '../pages/cart-modal/cart-modal.page';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +10,43 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  menu:any;
+  jumlahitem = 100;
+  ctritem = 0;
+  itemkembar = false;
+  // cart: any[] = [];
+  products: any[] = [];
+  cart = [];
+  cartItemCount!: BehaviorSubject<number>;
 
-  constructor() {}
+  constructor(private toastController: ToastController, private cartService: CartService, private modalCtrl: ModalController) {
+    
+  }
 
+  ngOnInit() {
+    this.products = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+    this.cartItemCount = this.cartService.getCartItemCount();
+  }
+
+  increment () {
+    this.jumlahitem++;
+  }
+  
+  decrement () {
+    this.jumlahitem--;
+
+  }
+
+  addToCart(product: any){
+    this.cartService.addProduct(product);
+  }
+
+  async openCart(){
+    let modal = await this.modalCtrl.create({
+      component: CartModalPage,
+      cssClass: 'cart-modal'
+    });
+    modal.present();
+  }
 }
