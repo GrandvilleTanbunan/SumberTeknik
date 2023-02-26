5bf9db3d2ccf81e532f07c203e5fe4ab392a307f
GrandvilleTanbunan <46751850+GrandvilleTanbunan@users.noreply.github.com>
2023-02-26 22:28:59 +0800

    logout

--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { FormBuilder } from '@angular/forms';
+import { LoadingController, AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profil',
@@ -8,14 +11,16 @@ import { AuthService } from '../services/auth.service';
 })
 export class ProfilPage implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private fb: FormBuilder, private loadingController:LoadingController, private alertController:AlertController, private router: Router) { }
 
   ngOnInit() {
   }
 
   logout()
   {
-    this.authService.logout();
+    this.authService.logout().then(()=>{
+      this.router.navigateByUrl("/", {replaceUrl: true});
+    });
   }
 
 }
